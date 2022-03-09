e35f4019063e968049221a2fab54110d1eba712c
Matheus Henrique <68704889+MatheusHenrique2000@users.noreply.github.com>
2022-03-09 19:23:14 -0300

    Delete start.js

--- framework/js/modelo_web/script/start.js
+++
@@ -1,9 +0,0 @@
-import Funcoes from "./modelo/funcoes.js"
-import Main from "./main.js"
-
-window.addEventListener("load", function(event) {
-    f = new Funcoes();
-    main = new Main();
-
-    console.log("Carregamento inicial");
-    });
